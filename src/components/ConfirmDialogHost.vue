cc5855bdf0282cffb7f6540d4d4566e40ef5df66
betaxab <betaxab@gmail.com>
2026-04-13 02:53:04 +0800

    fix: fix an issue where the ConfirmDialogHost was displayed at too low a z-index

--- src/components/ConfirmDialogHost.vue
+++ src/components/ConfirmDialogHost.vue
@@ -3,7 +3,8 @@ import { computed } from 'vue'
 import { storeToRefs } from 'pinia'
 import { useConfirmStore, type ConfirmDialogDescriptionSegment } from '@/stores/confirm'
 import { Button } from '@/components/ui/button'
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+import { Dialog, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+import { DialogContent, DialogOverlay, DialogPortal } from 'reka-ui'
 
 const confirmStore = useConfirmStore()
 const { open, options } = storeToRefs(confirmStore)
@@ -24,23 +25,30 @@ const segmentClass = (segment: ConfirmDialogDescriptionSegment) => {
 
 <template>
   <Dialog :open="open" @update:open="confirmStore.setOpen">
-    <DialogContent class="sm:max-w-md">
-      <DialogHeader>
-        <DialogTitle>{{ options.title }}</DialogTitle>
-        <DialogDescription>
-          <span
-            v-for="(segment, index) in descriptionSegments"
-            :key="index"
-            :class="[segmentClass(segment), segment.strong ? 'font-semibold' : 'font-normal']"
-          >
-            {{ segment.text }}
-          </span>
-        </DialogDescription>
-      </DialogHeader>
-      <DialogFooter>
-        <Button variant="outline" @click="confirmStore.cancel">{{ options.cancelText }}</Button>
-        <Button :variant="confirmVariant" @click="confirmStore.confirm">{{ options.confirmText }}</Button>
-      </DialogFooter>
-    </DialogContent>
+    <DialogPortal>
+      <DialogOverlay
+        class="fixed inset-0 z-[190] bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
+      />
+      <DialogContent
+        class="fixed left-1/2 top-1/2 z-[191] grid w-full max-w-lg -translate-x-1/2 -translate-y-1/2 gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 sm:rounded-lg sm:max-w-md"
+      >
+        <DialogHeader>
+          <DialogTitle>{{ options.title }}</DialogTitle>
+          <DialogDescription>
+            <span
+              v-for="(segment, index) in descriptionSegments"
+              :key="index"
+              :class="[segmentClass(segment), segment.strong ? 'font-semibold' : 'font-normal']"
+            >
+              {{ segment.text }}
+            </span>
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button variant="outline" @click="confirmStore.cancel">{{ options.cancelText }}</Button>
+          <Button :variant="confirmVariant" @click="confirmStore.confirm">{{ options.confirmText }}</Button>
+        </DialogFooter>
+      </DialogContent>
+    </DialogPortal>
   </Dialog>
 </template>
